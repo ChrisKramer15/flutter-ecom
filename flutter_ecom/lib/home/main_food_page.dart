26d7c45fd0f048da6df963b45a3bbfd871ff1eb0
Chris Kramer <chris@vetaip.com>
2022-03-10 16:31:37 -0600

    color config added, starting creation of text widget

--- flutter_ecom/lib/home/main_food_page.dart
+++ flutter_ecom/lib/home/main_food_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ecom/config/palette.dart';
 
 class MainFoodPage extends StatefulWidget {
   const MainFoodPage({Key? key}) : super(key: key);
@@ -11,20 +12,37 @@ class _MainFoodPageState extends State<MainFoodPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-            child: Container(
-                child: Row(
+        //body column container
+        body: Column(
       children: [
-        Column(
-          children: [Text("Country"), Text("City")],
-        ),
+        //unknown container
         Container(
-          height: 45,
-          width: 45,
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(15), color: Colors.blue),
-        )
+            //header container
+            child: Container(
+                margin: const EdgeInsets.only(top: 45, bottom: 15),
+                padding: const EdgeInsets.only(left: 20, right: 20),
+                //places header into row element
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    //left side of header
+                    Column(
+                      children: [Text("Country"), Text("City")],
+                    ),
+                    //button at right inside header
+                    Center(
+                      child: Container(
+                        height: 45,
+                        width: 45,
+                        child: const Icon(Icons.search, color: Colors.white),
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(15),
+                            color: AppColors.mainColor),
+                      ),
+                    )
+                  ],
+                )))
       ],
-    ))));
+    ));
   }
 }
---
+++ flutter_ecom/lib/widgets/big_text.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class BigText extends StatelessWidget {
+  const BigText({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text();
+  }
+}
